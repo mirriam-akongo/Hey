7d7f384ef0bf8c0d6da6559cdc246ac001f3a249
mirriam-akongo <jemmirriako@gmail.com>
2021-08-05 13:21:58 +0300

    initial commit: Basic sentence

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("It's going to be fun learning java");
+    }
+}
